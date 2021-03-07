1f8673ebb13513d3fc5e5e76c2613e2d6cbe02b2
Ramiro Iglesias <ramiro.igmun@gmail.com>
2021-03-07 16:54:19 +0100

    Parsing rules refactored

--- scr/app.js
+++ scr/app.js
@@ -3,11 +3,11 @@ import { Evolutioner } from "./evolutioner.js";
 import { generate3dArray } from "./arrayUtil.js";
 
 const length = 30;
-const rule = '3300';
-const [a, b, c, d] = rule;
+const rule = '3/0';
+const [survive, born] = rule.split('/');
 
 let currentGeneration = generate3dArray(length);
-const evolutioner = new Evolutioner({ survive: [a, b], born: [c, d] });
+const evolutioner = new Evolutioner({ survive, born });
 const renderer = new Renderer(13, 15);
 
 const gameLoop = () => {
